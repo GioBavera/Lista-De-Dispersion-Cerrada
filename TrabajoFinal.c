57a10dec83c5430cf5976a784951704ded185548
Gio_Bavera <149208089+GioBavera@users.noreply.github.com>
2023-11-30 20:33:39 -0300

    Update TrabajoFinal.c

--- TrabajoFinal.c
+++ TrabajoFinal.c
@@ -4,14 +4,14 @@
 #define NCasillas 25
 #define VACIO NULL
 
-static char *BORRADO = "**********"; // 10 asteriscos
+static char* BORRADO = "**********"; // 10 asteriscos
 
 typedef char **DICCIONARIO;
 
 
-DICCIONARIO CREAR ();  // aaa
-void DestruirTablaHash (DICCIONARIO); // aaa
-int h (char*); // aaa
+DICCIONARIO CREAR ();  
+void DestruirTablaHash (DICCIONARIO); 
+int h (char*); 
 int localiza (char*, DICCIONARIO);
 int localiza1 (char*, DICCIONARIO);
 int MIEMBRO (char*, DICCIONARIO);
@@ -52,95 +52,95 @@ int main(){
 //--------------------- FUNCIONES
 // Crea un diccionario vacío con NCasillas contenedores
 DICCIONARIO CREAR(){
-  DICCIONARIO A = (char**) malloc(NCasillas*sizeof(char*));
-  if (A == NULL) {
-    printf("Error al reservar memoria\n");
-  }
-  for (int i = 0; i < NCasillas; i++) {
-    A[i] = VACIO;
-  }
-  return A;
+    DICCIONARIO A = (char**) malloc(NCasillas*sizeof(char*));
+    for (int i = 0; i < NCasillas; i++) {
+        A[i] = VACIO;
+    }
+    return A;
 }
 
 // Libera la memoria del diccionario
 void DestruirTablaHash(DICCIONARIO A) {
-  for (int i = 0; i < NCasillas; i++) {
-    if (A[i] != VACIO) {
-      free(A[i]);
+    for (int i = 0; i < NCasillas; i++) {
+        if (A[i] != VACIO) {
+            free(A[i]);
+        }
     }
-  }
-  free(A);
+    free(A);
 }
+
 
 // Calcula el valor de dispersión de una cadena de caracteres
-// Suma los valores numéricos de cada letra y luego toma el residuo de la división con NCasillas[^11^][11]
+// Suma los valores numéricos de cada letra y luego toma el residuo de la división con NCasillas
 int h(char *x) {
-  int suma = 0;
-  for (int i = 0; i < 10; i++) {
-    suma += (int)x[i];
-  }
-  return suma % NCasillas;
+    int suma = 0;
+    for (int i = 0; i < 10; i++) {
+        suma += (int)x[i];
+    }
+    return suma % NCasillas;
 }
 
-// Busca el índice del contenedor donde se encuentra x o un contenedor vacío
-// Se detiene si encuentra x o un contenedor vacío, o si revisa toda la tabla[^14^][14]
+// Busca el indice del contenedor (Primero x, despues vacio) y devuelve su direccion.
 int localiza(char *x, DICCIONARIO A) {
-  int inicial = h(x); // guarda el valor de dispersión de x
-  int i = 0;          // cuenta el número de contenedores examinados
-  while (i < NCasillas && A[(inicial + i) % NCasillas] != x &&
-         A[(inicial + i) % NCasillas] != VACIO) {
-    i++;
-  }
-  return (inicial + i) % NCasillas;
+    int inicial = h(x);
+    int i = 0;
+
+    if (A[inicial] == VACIO) {
+        return inicial;
+    }
+
+    while (i < NCasillas && A[(inicial + i) % NCasillas] != VACIO) {
+        i++;
+    }
+
+    return (inicial + i) % NCasillas;
 }
 
-// Como localiza, pero también se detiene en una entrada con BORRADO[^15^][15]
+// Busca el indice del contenedor (Primero x, despues vacio o borrado) y devuelve su direccion.
 int localiza1(char *x, DICCIONARIO A) {
-  int inicial = h(x);
-  int i = 0;
-  while (i < NCasillas && A[(inicial + i) % NCasillas] != x &&
-         A[(inicial + i) % NCasillas] != VACIO &&
-         A[(inicial + i) % NCasillas] != BORRADO) {
-    i++;
-  }
-  return (inicial + i) % NCasillas;
+    int inicial = h(x); 
+    int i = 0;          
+
+    if (A[inicial] == VACIO || A[inicial] == BORRADO) {
+        return inicial;
+    }
+
+    while (i < NCasillas && A[(inicial + i) % NCasillas] != VACIO && A[(inicial + i) % NCasillas] != BORRADO) {
+        i++;
+    }
+
+    return (inicial + i) % NCasillas;
 }
 
 // Devuelve 1 si x es miembro del diccionario, 0 en caso contrario
 int MIEMBRO(char *x, DICCIONARIO A) {
-  int index = localiza(x, A);
-  if (strcmp(A[index], x) == 0) {
-    return 1;
-  } else {
-    return 0;
-  }
+    int index = localiza(x, A);
+    if (strcmp(A[index], x) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 // Inserta x en el diccionario si no está ya
 // Busca una localidad vacía o con BORRADO para insertar x
 void INSERTA(char *x, DICCIONARIO A) {
-  if (A[localiza(x, A)] == x) {
-    return; // x ya está en A
-  }
-  int contenedor = localiza1(x, A);
-  if (A[contenedor] == VACIO || A[contenedor] == BORRADO) {
-    A[contenedor] = (char *)malloc(11 * sizeof(char)); // reserva espacio para 10 caracteres y el '\0'
-    if (A[contenedor] == NULL) {
-      printf("Error al reservar memoria\n");
-      exit(1);
+    int index = localiza1(x, A); 
+
+    if (A[index] == VACIO || A[index] == BORRADO) {
+        A[index] = (char *)malloc((strlen(x) + 1) * sizeof(char)); 
+        strcpy(A[index], x); // Copia x en el contenedor
+    } else {
+        printf("ERROR. Diccionario Lleno.\n");
     }
-    strcpy(A[contenedor], x); // copia x en el contenedor
-  } else {
-    printf("INSERTA falló, la tabla está llena\n");
-  }
 }
 
-// Suprime x del diccionario si está
-// Coloca BORRADO en el contenedor que contiene x
+// Coloca BORRADO en el contenedor de X
 void SUPRIME(char *x, DICCIONARIO A) {
-  int contenedor = localiza(x, A);
-  if (A[contenedor] == x) {
-    free(A[contenedor]);       // libera el espacio de x
-    A[contenedor] = BORRADO;   // marca el contenedor como borrado
-  }
+    int index = localiza(x, A); 
+
+    if (strcmp(A[index], x) == 0) {
+        A[index] = BORRADO;   
+    }
 }
+
